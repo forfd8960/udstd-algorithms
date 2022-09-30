5e134fe5d3f07815b6fff6fc29335b8624668abb
forfd8960 <forfd8960@gmail.com>
2022-09-30 19:44:57 +0800

    Find Kth large element

--- quick_sort.py
+++ quick_sort.py
@@ -12,14 +12,14 @@ def quic_sort(arr, low, high):
     if low >= high:
         return arr
 
-    pivot = partion(arr, low, high)
+    pivot = partition(arr, low, high)
     quic_sort(arr, 0, pivot-1)
     quic_sort(arr, pivot+1, high)
 
     return arr
 
 
-def partion(arr: list[int], low, high) -> int:
+def partition(arr: list[int], low, high) -> int:
     if len(arr) <= 1:
         return 0
     
@@ -50,3 +50,22 @@ def partion(arr: list[int], low, high) -> int:
 
     arr[i-1], arr[low] = arr[low], arr[i-1]
     return i-1
+
+# find kth large element in the arr
+def find_kth_element(arr: list[int], k: int) -> int:
+    if k > len(arr) or k <= 0:
+        return -1
+
+    xk = len(arr) - (k - 1) - 1
+    return find_kth_in_arr(arr, xk, 0, len(arr)-1)
+
+
+def find_kth_in_arr(arr: list[int], k, low, high) ->  int:
+    pivot = partition(arr, low, high)
+    if k < pivot:
+        return find_kth_in_arr(arr, k, low, pivot-1)
+    
+    if k > pivot:
+        return find_kth_in_arr(arr, k, pivot+1, high)
+    
+    return arr[pivot]
