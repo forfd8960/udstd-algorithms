e2c73be974cc8d76b54c5fbb5630878f67bd698f
forfd8960 <forfd8960@gmail.com>
2022-10-04 16:36:25 +0800

    Binary search

--- binary_search.py
+++ binary_search.py
@@ -1,17 +1,35 @@
 #python3
 
-
+# https://time.geekbang.org/column/article/42520
 def binary_search(arr, n, k):
     index = -1
-    left, right = 0, n - 1
-    while right >= left:
-        mid = (left + right) // 2
+    low, high = 0, n - 1
+    while low <= high:
+        mid = low + ((high - low) >> 1)
         if arr[mid] < k:
-            left = mid + 1
+            low = mid + 1
         elif arr[mid] > k:
-            right = mid - 1
+            high = mid - 1
         else:
             index = mid
             return index
         
     return index
+
+
+def my_sqrt(n) -> float:
+    if n <= 0:
+        return 0.0
+    
+    low, high = 0, n
+    mid = n / 2
+    
+    while abs(mid ** 2 - n) > 0.000001:
+        if mid ** 2 < n:
+            low = mid
+        else:
+            high = mid
+            
+        mid = (low + high) / 2
+
+    return mid
